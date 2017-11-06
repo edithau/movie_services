1b7451aa1c4561cbc778c43f8711feafe00d48c5
edithau <edith.au@gmail.com>
2017-11-06 12:16:27 -0800

    fixed endpoints, fixed routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,20 @@
 class MoviesController < ApplicationController
 
   def index
-    movies = Movie.limit(200).select('id,name,year,genre')
+
+    if params[:ids]
+      ids = params[:ids].split(',')
+      movies = Movie.where('id' => ids).select('id,name,year,genre')
+    else
+      movies = Movie.limit(200).select('id,name,year,genre')
+    end
     render json: movies, status: 200
   end
+
+  def show
+    movies = Movie.where(['id = ?', params[:id]]).select('id,name,year,genre').first
+    render json: movies, status: 200
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/recent_rented_movies' => 'movies#index', as: :recent_rented_movies
+  resources :movies, only: [:show, :index]
 
 end
