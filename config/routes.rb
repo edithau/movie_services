36a00480e5673a87413386ce48094d6ea3efc361
edithau <edith.au@gmail.com>
2017-11-06 22:35:37 -0800

    added endpoint to support movie queue services cache preload

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :movies, only: [:show, :index]
+  get '/popular_movies' => 'movies#index', as: :popular_movies
 
 end
